341a21b29e4df21eea479142dbeaa2dc3da7c814
cgarcia1196 <cgarcia1196@gmail.com>
2025-04-23 10:47:14 -0700

    completed main part of project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,13 @@ import Home from "./pages/Home.jsx"
 import Favorites from './pages/Favorites.jsx'
 import NavBar from './components/NavBar.jsx'
 import {Routes, Route} from "react-router-dom"
+import {MovieProvider} from "./contexts/MovieContext"
 
 function App() {
   const movieNumber = 2
 
   return (
-    <div>
+    <MovieProvider>
       <NavBar/>
       <main className="main-content">
         <Routes>
@@ -16,7 +17,7 @@ function App() {
           <Route path="/favorites" element={<Favorites />}/>
         </Routes>  
       </main>
-    </div>
+    </MovieProvider>
   )
   
 }
