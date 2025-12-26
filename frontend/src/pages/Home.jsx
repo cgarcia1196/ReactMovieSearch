0d93bf6dec5a9b55e868d329d43eae67ac568e0b
cgarcia1196 <cgarcia1196@gmail.com>
2025-12-26 09:36:00 -0800

    fixed displaying movies after search

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,79 +1,81 @@
-import MovieCard from "../components/MovieCard"
-import { useState, useEffect } from "react"
-import "../css/Home.css"
-import {searchMovies, getPopularMovies} from "../services/api"
+import MovieCard from "../components/MovieCard";
+import { useState, useEffect } from "react";
+import "../css/Home.css";
+import { searchMovies, getPopularMovies } from "../services/api";
 
 function Home() {
-    const [searchQuery, setSearchQuery] = useState("")
-    const [movies, setMovies] = useState([])
-    const [error, setError] = useState(null)
-    const [loading, setLoading] = useState(true)
+  const [searchQuery, setSearchQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const loadPopularMovies = async () => {
-            try{
-                const popularMovies = await getPopularMovies()
-                setMovies(popularMovies)
-            }catch (err){
-                console.log(err)
-                setError("Failed to load movies...")
-            }
-            finally{
-                setLoading(false)
-            }  
-        }
-        loadPopularMovies()
-    }, [])
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies);
+      } catch (err) {
+        console.log(err);
+        setError("Failed to load movies...");
+      } finally {
+        setLoading(false);
+        console.log(movies);
+      }
+    };
+    loadPopularMovies();
+  }, []);
 
-    const handleSearch = async (e) => {
-        e.preventDefault()
-        //remove empy spaces before and after seach query
-        if (!searchQuery.trim()) return
-        if(loading) return
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    //remove empy spaces before and after seach query
+    if (!searchQuery.trim()) return;
+    if (loading) return;
 
-        setLoading(true)
-        try{
-            const searchResults = await searchMovies(searchQuery)
-            setMovies(searchResults)
-            setError(null)
-        } catch(err){
-            console.log(err)
-            setError("Failed to seach movies")
-        } finally{
-            setLoading(false)
-        }
+    setLoading(true);
+    try {
+      const searchResults = await searchMovies(searchQuery);
+      setMovies(searchResults);
+      setSearchQuery("");
+      setError(null);
+    } catch (err) {
+      console.log(err);
+      setError("Failed to seach movies");
+    } finally {
+      setLoading(false);
     }
+  };
 
-    return (
-        <div className="home">
-            <form onSubmit={handleSearch} className="search-form">
-                <input 
-                    type="text" 
-                    placeholder="Search for movies... " 
-                    className="search-input" 
-                    value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)}
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies... "
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
 
-                />
-                <button type="submit" className="search-button">Search</button>
-            </form>
+      {error && <div className="error-message">{error}</div>}
 
-            {error && <div className="error-message">{error}</div>}
-
-            {loading ? 
-                <div className="loading">Loading...</div>
-                :
-                <div className="movies-grid">
-                    {movies.map(
-                        (movie) => 
-                            movie.title.toLowerCase().startsWith(searchQuery) && 
-                            <MovieCard movie={movie} key={movie.id}/>
-                    )}
-                </div>
-            }
-            
+      {loading ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        <div className="movies-grid">
+          {movies.map(
+            (movie) =>
+              movie.title.toLowerCase().startsWith(searchQuery) && (
+                <MovieCard movie={movie} key={movie.id} />
+              )
+          )}
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,14 +1,17 @@
-const API_KEY = ""
-const BASE_URL = "https://api.themoviedb.org/3"
+const API_KEY = import.meta.env.VITE_THEMOVIEDB_API_KEY;
+const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
-    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
-    const data = await response.json()
-    return data.results
-}
+  console.log("api key: ", API_KEY);
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
 
 export const searchMovies = async (query) => {
-    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`)
-    const data = await response.json()
-    return data.results
-}
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`
+  );
+  const data = await response.json();
+  return data.results;
+};
